6b094408aa8b8c641e5f8d905d4f5df1c6237c47
Jarad DeLorenzo <jarad@splashthat.com>
2014-10-16 20:03:27 -0400

    Added lots of fonts. Lots of styling:

--- src/ElleOL/FrontEndBundle/Controller/HomeController.php
+++ src/ElleOL/FrontEndBundle/Controller/HomeController.php
@@ -9,11 +9,11 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 class HomeController extends Controller
 {
     /**
-     * @Route("/hello/{name}")
+     * @Route("/home")
      * @Template()
      */
-    public function indexAction($name)
+    public function indexAction()
     {
-        return array('name' => $name);
+    	return array();
     }
 }
